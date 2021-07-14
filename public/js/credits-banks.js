31bd2a91c374d2f2f5b51632bfb4658ba415ef48
Andrey Vasilyuk <a.vasilyuk@leadgid.ru>
2021-07-14 16:37:41 +0300

    edits

--- public/js/credits-banks.js
+++ public/js/credits-banks.js
@@ -26,10 +26,10 @@ xhr.onreadystatechange = function() {
                     '                    <div class="offers--logo">\n' +
                     '                        <img src="'+res[prop].img+'" class="offers--img" alt="'+res[prop].name+'">\n' +
                     '                    </div>\n' +
-                    '                    <div class="offers--1">Від '+res[prop].rateFrom+'%</div>\n' +
-                    '                    <div class="offers--2">До '+new Intl.NumberFormat('ru-RU').format(res[prop].amountUp)+'₴</div>\n' +
-                    '                    <div class="offers--3">'+res[prop].operatingMode+'</div>\n' +
-                    '                    <div class="offers--4">'+res[prop].endorsements+'</div>\n' +
+                    '                    <div class="offers--1"><div class="offers--signature">Процентна ставка</div>Від '+res[prop].rateFrom+'%</div>\n' +
+                    '                    <div class="offers--2"><div class="offers--signature">Сума</div>До '+new Intl.NumberFormat('ru-RU').format(res[prop].amountUp)+'₴</div>\n' +
+                    '                    <div class="offers--3"><div class="offers--signature">Графік роботи</div>'+res[prop].operatingMode+'</div>\n' +
+                    '                    <div class="offers--4"><div class="offers--signature"></div>'+res[prop].endorsements+'</div>\n' +
                     '                    <a href="'+res[prop].link+'" target="_blank" class="btn offers--btn">ПОДАТИ ЗАЯВКУ</a>\n' +
                     '                </div>';
             }else{
@@ -37,10 +37,10 @@ xhr.onreadystatechange = function() {
                     '                    <div class="offers--logo">\n' +
                     '                        <img src="'+res[prop].img+'" class="offers--img" alt="'+res[prop].name+'">\n' +
                     '                    </div>\n' +
-                    '                    <div class="offers--1">От '+res[prop].rateFrom+'%</div>\n' +
-                    '                    <div class="offers--2">До '+new Intl.NumberFormat('ru-RU').format(res[prop].amountUp)+'₴</div>\n' +
-                    '                    <div class="offers--3">'+res[prop].operatingMode+'</div>\n' +
-                    '                    <div class="offers--4">'+res[prop].endorsements+'</div>\n' +
+                    '                    <div class="offers--1"><div class="offers--signature">Процентная ставка</div>От '+res[prop].rateFrom+'%</div>\n' +
+                    '                    <div class="offers--2"><div class="offers--signature">Сумма</div>До '+new Intl.NumberFormat('ru-RU').format(res[prop].amountUp)+'₴</div>\n' +
+                    '                    <div class="offers--3"><div class="offers--signature">График работы</div>'+res[prop].operatingMode+'</div>\n' +
+                    '                    <div class="offers--4"><div class="offers--signature"></div>'+res[prop].endorsements+'</div>\n' +
                     '                    <a href="'+res[prop].link+'" target="_blank" class="btn offers--btn">ПОДАТЬ ЗЯВКУ</a>\n' +
                     '                </div>';
             }
