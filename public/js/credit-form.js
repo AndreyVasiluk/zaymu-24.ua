7337121e9162bee81fcb1e895921cd6741d6bb10
Andrey Vasilyuk <a.vasilyuk@leadgid.ru>
2021-09-06 12:26:10 +0300

    fdf

--- public/js/credit-form.js
+++ public/js/credit-form.js
@@ -209,7 +209,7 @@ function addProcess(){
                      if(json.status===200){
                          form.reset();
                          document.cookie = 'user= {"email": "'+json.email+'", "name": "'+json.name+'", "tel": "'+json.tel+'"}; max-age=1800';
-                         location.href = '/'+locale+'/successful-registration'
+                         location.href = '/'+locale+'/credits-banks'
                      }else{
                          if(locale=='ua'){
                              formError.innerHTML = "Email вже існує";
